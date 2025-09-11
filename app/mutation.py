a6dcc50cb9d8ebbad12301f70ed97acd9deb9a50
Charlie-Warren <c_warren1@live.com>
2025-09-11 16:25:22 +0100

    improved docstrings and added GraphQLErrors

--- app/mutation.py
+++ app/mutation.py
@@ -3,6 +3,7 @@ from database import TaskORM, SessionLocal
 from task import Task
 from uuid import uuid4
 from datetime import datetime
+from strawberry.exceptions import GraphQLError
 
 
 @strawberry.type
@@ -11,6 +12,12 @@ class Mutation:
     def add_task(self, title: str) -> Task:
         """
         Create a new task.
+
+        Args:
+            title: The title of the task.
+
+        Returns:
+            The newly created task.
         """
         with SessionLocal() as db:
             task = TaskORM(
@@ -25,14 +32,20 @@ class Mutation:
         return task
 
     @strawberry.mutation
-    def toggle_task(self, id: strawberry.ID) -> Task | None:
+    def toggle_task(self, id: strawberry.ID) -> Task:
         """
         Toggle a task between completed and not completed.
+
+        Args:
+            id: The ID of the task to toggle.
+
+        Returns:
+            The updated task.
         """
         with SessionLocal() as db:
             task = db.query(TaskORM).filter(TaskORM.id == str(id)).first()
             if not task:
-                return None
+                raise GraphQLError(f"Task with ID \"{id}\" not found")
             task.completed = not task.completed
             task.updated_at = datetime.now()
             db.commit()
@@ -40,14 +53,20 @@ class Mutation:
         return task
 
     @strawberry.mutation
-    def delete_task(self, id: strawberry.ID) -> Task | None:
+    def delete_task(self, id: strawberry.ID) -> Task:
         """
         Delete a task, returning the deleted task.
+
+        Args:
+            id: The ID of the task to delete.
+
+        Returns:
+            The deleted task.
         """
         with SessionLocal() as db:
             task = db.query(TaskORM).filter(TaskORM.id == str(id)).first()
             if not task:
-                return None
+                raise GraphQLError(f"Task with ID \"{id}\" not found")
             deleted_task_data = Task(
                 id=task.id,
                 title=task.title,
--- app/query.py
+++ app/query.py
@@ -9,6 +9,12 @@ class Query:
     def tasks(self) -> list[Task]:
         """
         Return a list of all tasks.
+
+        Args:
+            None
+
+        Returns:
+            A list of all tasks.
         """
         with SessionLocal() as db:
             return db.query(TaskORM).all()
@@ -17,6 +23,12 @@ class Query:
     def task(self, id: strawberry.ID) -> Task | None:
         """
         Get a particular Task by ID.
+
+        Args:
+            id: The ID of the task to retrieve.
+
+        Returns:
+            The task with the specified ID, or None if not found.
         """
         with SessionLocal() as db:
             return db.query(TaskORM).filter(TaskORM.id == str(id)).first()
