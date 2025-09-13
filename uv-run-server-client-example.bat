4dd07750f173ad0ff91acd4ba036714b640cd621
Charlie-Warren <c_warren1@live.com>
2025-09-13 15:44:06 +0100

    Update uv-run-server-client-example.bat

--- uv-run-server-client-example.bat
+++ uv-run-server-client-example.bat
@@ -4,5 +4,5 @@ start "Server" cmd /k uv run -m app.main
 
 timeout /t 5 /nobreak >nul
 
-REM Run example.py in a new window
+REM Run client_example.py in a new window
 start "Example Client" cmd /k uv run client_example.py
